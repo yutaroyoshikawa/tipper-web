898e5ad53c4317eb13fdf3c7b8488ba5c35b5418
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-02-23 18:34:29 +0900

    envラベル修正

--- src/utils/initializeFirebase.ts
+++ src/utils/initializeFirebase.ts
@@ -5,12 +5,12 @@ export const initializeFirebase = (): void => {
   if (firebase.app.length > 0) {
     firebase.initializeApp({
       apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messageSenderId: process.env.MESSAGE_SENDER_ID,
-      appId: process.env.APP_ID,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messageSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
     });
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
   }
