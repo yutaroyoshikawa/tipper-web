9ee8a5b4404744d0d02cf2030045b23f2e02a3cc
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-02-23 18:21:45 +0900

    不要な型の削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NextPage } from "next";
 
-const IndexPage: NextPage<IndexPageProps> = () => <h1>Hello World!</h1>;
+const IndexPage: NextPage = () => <h1>Hello World!</h1>;
 
 export default IndexPage;
